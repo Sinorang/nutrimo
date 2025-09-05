ad2f11ffd98d90a53d93eb54031ace58f5782373
sinorang <sinorang@huray.net>
2025-09-05 07:22:11 +0000

    init: 프로젝트 초기 설정
    - spring application 정상 작동 확인 필요
    - 확인 후 기능 구현 예정

---
+++ api/src/main/java/com/github/sinorang/NutrimoApplication.java
@@ -0,0 +1,12 @@
+package com.github.sinorang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NutrimoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NutrimoApplication.class, args);
+    }
+}
